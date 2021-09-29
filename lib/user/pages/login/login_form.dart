6e61c757d552ed1688ac00fa52d850e2fe4eb97a
Thanh <thph@stofa.dk>
2021-09-29 15:58:48 +0200

    import login widget from main

---
+++ lib/user/pages/login/login_form.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[TextField()],
+      ),
+    );
+  }
+}
+
+class TextField extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Text("hello");
+  }
+}
