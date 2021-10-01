b4fe4a5bcaf02aeb20eb995fc71eaf722d1a0a99
Uniowl <AlexSmith@live.dk>
2021-10-01 10:04:53 +0200

    Fixed typo in import path

--- lib/user/pages/login/login_form.dart
+++ lib/user/pages/login/login_form.dart
@@ -1,14 +1,14 @@
-import 'package:beefitmember_application/shared/shared_widgets/textfield.dart';
+import 'package:beefitmember_application/shared/widgets/textfield.dart';
 import 'package:flutter/material.dart';
 
 class Login extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: <Widget>[
-          TextFieldInput('Username', 200.0), TextFieldInput('Password', 200.0)
+          TextFieldInput('Username', 200.0),
+          TextFieldInput('Password', 200.0)
         ],
       ),
     );
