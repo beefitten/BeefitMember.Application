3e179eeb1c88d897969cd96f83155b14a28485d0
Thanh <thph@stofa.dk>
2021-10-04 11:11:45 +0200

    lost found

---
+++ lib/user/bloc/login_state.dart
@@ -0,0 +1,20 @@
+import 'package:equatable/equatable.dart';
+
+class LoginState extends Equatable {
+  @override
+  List<Object> get props => [];
+}
+
+class LoginInitState extends LoginState{}
+
+class LoginLoadingState extends LoginState{}
+
+class UserLoginSuccessState extends LoginState{}
+
+class AdminLoginSuccessState extends LoginState{}
+
+class LoginErrorState extends LoginState{
+  final String message;
+
+  LoginErrorState({required this.message});
+}
