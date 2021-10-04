53a17d59af63e4527bc93dbc54951e6beb0435ab
Uniowl <mobilerobotcat@gmail.com>
2021-10-04 18:32:33 +0200

    Deleted redundant code
    The class had been inserted as an inspiration peace and was never meant
    to be used

--- lib/user/pages/pre_login/widgets/CarouselWidget.dart
+++ lib/user/pages/pre_login/widgets/CarouselWidget.dart
@@ -15,23 +15,3 @@ class CarouselWidget extends StatelessWidget {
     );
   }
 }
-
-class ImageBanner extends StatelessWidget {
-  final String _assetPath;
-
-  ImageBanner(this._assetPath);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      constraints: BoxConstraints.expand(
-        height: 400.0,
-      ),
-      decoration: BoxDecoration(color: Colors.grey),
-      child: Image.asset(
-        _assetPath,
-        fit: BoxFit.cover,
-      ),
-    );
-  }
-}
