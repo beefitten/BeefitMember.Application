958d4602c5e89dd26be56c66e7569e0210b1eb97
Uniowl <AlexSmith@live.dk>
2021-10-06 16:13:17 +0200

    Fixed bug with "home: PreLogin"

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:beefitmember_application/user/bloc/login_bloc.dart';
 import 'package:beefitmember_application/user/bloc/login_state.dart';
 import 'package:beefitmember_application/user/pages/Contact.dart';
@@ -6,6 +5,8 @@ import 'package:beefitmember_application/user/pages/login/login_form.dart';
 import 'package:beefitmember_application/user/service/login_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'user/pages/pre_login/pre_login.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,16 +17,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-        BlocProvider(create: (context)=> LoginBLoc(LoginInitState(), LoginService()))
+        BlocProvider(
+            create: (context) => LoginBLoc(LoginInitState(), LoginService()))
       ],
       child: MaterialApp(
         title: 'Beefit Member',
         routes: {
-          '/' : (context) => Login(),
-          '/contacts' : (context) => Contact(),
+          '/': (context) => PreLogin(),
+          '/contacts': (context) => Contact(),
         },
       ),
-      home: PreLogin(),
     );
   }
 }
