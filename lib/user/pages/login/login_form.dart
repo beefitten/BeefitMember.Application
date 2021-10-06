944806807391496c63c732e337c279fa9e2279ae
Uniowl <AlexSmith@live.dk>
2021-10-06 16:18:58 +0200

    Fixed incorrect pathname

--- lib/user/pages/login/login_form.dart
+++ lib/user/pages/login/login_form.dart
@@ -1,6 +1,6 @@
 import 'dart:developer';
 
-import 'package:beefitmember_application/shared/shared_widgets/textfield.dart';
+import 'package:beefitmember_application/shared/widgets/textfield.dart';
 import 'package:beefitmember_application/user/bloc/login_bloc.dart';
 import 'package:beefitmember_application/user/bloc/login_events.dart';
 import 'package:beefitmember_application/user/bloc/login_state.dart';
@@ -9,9 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 
-
-class Login extends StatefulWidget{
-
+class Login extends StatefulWidget {
   @override
   _UserLoginState createState() => _UserLoginState();
 }
@@ -23,12 +21,13 @@ class _UserLoginState extends State<Login> {
   late LoginBLoc loginBLoc;
   bool _obscureText = true;
 
-  void _toggleObscureText(){
+  void _toggleObscureText() {
     setState(() {
       _obscureText = !_obscureText;
     });
   }
-  final String logo_path = "lib/shared/shared_assets/images/logo.svg";
+
+  final String logo_path = "lib/shared/assets/images/logo.svg";
 
   @override
   void initState() {
@@ -37,42 +36,49 @@ class _UserLoginState extends State<Login> {
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     final logo = Center(
-      child: SvgPicture.asset(logo_path,)
-    );
+        child: SvgPicture.asset(
+      logo_path,
+    ));
 
     final headContainer = Container(
       child: logo,
-      height: MediaQuery.of(context).size.height *0.3,
+      height: MediaQuery.of(context).size.height * 0.3,
       color: Color.fromRGBO(242, 245, 255, 1),
-      margin: EdgeInsets.only(bottom: MediaQuery.of(context).size.height *0.03),
+      margin:
+          EdgeInsets.only(bottom: MediaQuery.of(context).size.height * 0.03),
     );
 
     final welcomeText = Container(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children:[
-          Text("Welcome!",
-        style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 24,),),
+        children: [
+          Text(
+            "Welcome!",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 24,
+            ),
+          ),
           SizedBox(height: 6.0),
-          Text("Please enter your email and password",
-          style: TextStyle(
-            fontSize: 13,
-            color: Color.fromRGBO(138, 141, 178, 1)),)
+          Text(
+            "Please enter your email and password",
+            style: TextStyle(
+                fontSize: 13, color: Color.fromRGBO(138, 141, 178, 1)),
+          )
         ],
       ),
     );
 
     final msg = BlocBuilder<LoginBLoc, LoginState>(
       builder: (context, state) {
-        if(state is LoginErrorState){
+        if (state is LoginErrorState) {
           return Text(state.message);
-        }
-        else if (state is LoginLoadingState){
-          return Center(child: CircularProgressIndicator(),);
+        } else if (state is LoginLoadingState) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
         }
         return Container();
       },
@@ -95,55 +101,57 @@ class _UserLoginState extends State<Login> {
         5.0,
         TextInputType.emailAddress);
 
-
     final passwordInput = Container(
       child: Column(
         children: [
-        TextField(
-        controller: password,
-        autofocus: false,
-        obscureText: _obscureText,
-        decoration: InputDecoration(
-            filled: true,
-            fillColor: Color.fromRGBO(247, 247, 252, 1),
-            hintStyle: TextStyle(
-                color: Color(0xFF666666)
-            ),
-            hintText: 'Password',
-            contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-            border: OutlineInputBorder(borderRadius: BorderRadius.circular(5.0)),
-            suffix: InkWell(
-              onTap: _toggleObscureText,
-              child: Icon(_obscureText? Icons.visibility : Icons.visibility_off),
-            )
-        ),
-      ),
+          TextField(
+            controller: password,
+            autofocus: false,
+            obscureText: _obscureText,
+            decoration: InputDecoration(
+                filled: true,
+                fillColor: Color.fromRGBO(247, 247, 252, 1),
+                hintStyle: TextStyle(color: Color(0xFF666666)),
+                hintText: 'Password',
+                contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0)),
+                suffix: InkWell(
+                  onTap: _toggleObscureText,
+                  child: Icon(
+                      _obscureText ? Icons.visibility : Icons.visibility_off),
+                )),
+          ),
         ],
       ),
     );
 
     final loginButton = Padding(
-        padding: EdgeInsets.symmetric(vertical: 16.0),
+      padding: EdgeInsets.symmetric(vertical: 16.0),
       // ignore: deprecated_member_use
       child: RaisedButton(
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(5),
         ),
         onPressed: () {
-          loginBLoc.add(LoginButtonPressed(email: email.text, password: password.text));
+          loginBLoc.add(
+              LoginButtonPressed(email: email.text, password: password.text));
         },
         padding: EdgeInsets.all(12),
         color: Color.fromRGBO(6, 62, 249, 1),
-        child: Text('Login', style: TextStyle(color: Colors.white),),
+        child: Text(
+          'Login',
+          style: TextStyle(color: Colors.white),
+        ),
       ),
     );
 
     return Scaffold(
       backgroundColor: Colors.white,
       body: BlocListener<LoginBLoc, LoginState>(
-        listener: (context, state){
-          if(state is UserLoginSuccessState){
-           Navigator.pushNamed(context, '/contacts');
+        listener: (context, state) {
+          if (state is UserLoginSuccessState) {
+            Navigator.pushNamed(context, '/contacts');
           }
         },
         child: ListView(
@@ -162,6 +170,5 @@ class _UserLoginState extends State<Login> {
         ),
       ),
     );
-
   }
 }
