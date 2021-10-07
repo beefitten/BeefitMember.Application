88adaa8939fda01c90cc46b25f2c527dce67579c
Uniowl <AlexSmith@live.dk>
2021-10-07 10:56:50 +0200

    Added index indicator under carousel

--- lib/user/pages/pre_login/widgets/CarouselWidget.dart
+++ lib/user/pages/pre_login/widgets/CarouselWidget.dart
@@ -1,28 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
-class CarouselWidget extends StatelessWidget {
+class CarouselWidget extends StatefulWidget {
+  @override
+  State<CarouselWidget> createState() => _CarouselWidgetState();
+}
+
+class _CarouselWidgetState extends State<CarouselWidget> {
   final images = [
     "lib/user/pages/pre_login/assets/tree.jpg",
     "lib/user/pages/pre_login/assets/plane.jpg",
     "lib/user/pages/pre_login/assets/jura.png",
   ];
 
+  int activeIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: CarouselSlider.builder(
-        options: CarouselOptions(
-          height: 550,
-          autoPlay: true,
-          viewportFraction: 1,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        CarouselSlider.builder(
+          options: CarouselOptions(
+            height: 550,
+            autoPlay: true,
+            viewportFraction: 1,
+            onPageChanged: (index, reason) =>
+                setState(() => activeIndex = index),
+          ),
+          itemCount: images.length,
+          itemBuilder: (context, index, realIndex) {
+            final image = images[index];
+            return buildImage(image, index);
+          },
         ),
-        itemCount: images.length,
-        itemBuilder: (context, index, realIndex) {
-          final image = images[index];
-          return buildImage(image, index);
-        },
-      ),
+        Padding(
+          padding: const EdgeInsets.all(14.0),
+          child: buildIndicator(),
+        ),
+      ],
     );
   }
 
@@ -33,5 +50,10 @@ class CarouselWidget extends StatelessWidget {
           image,
           fit: BoxFit.cover,
         ),
+      );
+
+  Widget buildIndicator() => AnimatedSmoothIndicator(
+        activeIndex: activeIndex,
+        count: images.length,
       );
 }
