6293a0604cb88ba4e63801306c8dec4cbc1610e8
Uniowl <AlexSmith@live.dk>
2021-10-07 08:48:20 +0200

    Deleted unusued imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:beefitmember_application/user/bloc/login_bloc.dart';
 import 'package:beefitmember_application/user/bloc/login_state.dart';
 import 'package:beefitmember_application/user/pages/Contact.dart';
-import 'package:beefitmember_application/user/pages/login/login_form.dart';
 import 'package:beefitmember_application/user/service/login_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
