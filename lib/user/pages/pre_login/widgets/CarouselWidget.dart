eae581b26f5a48d477155b665f323059ba86fc8a
Uniowl <AlexSmith@live.dk>
2021-10-07 09:45:21 +0200

    Added carousel_slider to project.
    This allows for the import of the carousel_slider library

--- lib/user/pages/pre_login/widgets/CarouselWidget.dart
+++ lib/user/pages/pre_login/widgets/CarouselWidget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class CarouselWidget extends StatelessWidget {
   const CarouselWidget({Key? key}) : super(key: key);
