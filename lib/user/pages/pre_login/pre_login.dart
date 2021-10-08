ff60ac4de749f3e09a680ec8623848068dc8bbf1
Uniowl <AlexSmith@live.dk>
2021-10-08 15:18:43 +0200

    Added 120Hz screensupport for pre-login

--- lib/user/pages/pre_login/pre_login.dart
+++ lib/user/pages/pre_login/pre_login.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
 import 'widgets/CarouselWidget.dart';
 import 'widgets/ButtonGroupWidget.dart';
+import '../../../utils/refreshRate.dart';
 
-class PreLogin extends StatelessWidget {
+class PreLogin extends StatefulWidget {
   // This widget is the root of your application.
+  @override
+  State<PreLogin> createState() => _PreLoginState();
+}
+
+class _PreLoginState extends State<PreLogin> {
+  @override
+  void initState() {
+    RefreshRate.setOptimalDisplayMode();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
