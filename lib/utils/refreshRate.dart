c6f61ac5434ea7247b0e7d43c0df070c3afb45aa
Uniowl <AlexSmith@live.dk>
2021-10-08 14:33:41 +0200

    Prettified

--- lib/utils/refreshRate.dart
+++ lib/utils/refreshRate.dart
@@ -5,15 +5,15 @@ class RefreshRate {
     final List<DisplayMode> supported = await FlutterDisplayMode.supported;
     final DisplayMode active = await FlutterDisplayMode.active;
 
-    final List<DisplayMode> sameResolution = supported.where(
-            (DisplayMode m) => m.width == active.width
-            && m.height == active.height).toList()..sort(
-            (DisplayMode a, DisplayMode b) =>
-            b.refreshRate.compareTo(a.refreshRate));
+    final List<DisplayMode> sameResolution = supported
+        .where((DisplayMode m) =>
+            m.width == active.width && m.height == active.height)
+        .toList()
+      ..sort((DisplayMode a, DisplayMode b) =>
+          b.refreshRate.compareTo(a.refreshRate));
 
-    final DisplayMode mostOptimalMode = sameResolution.isNotEmpty
-        ? sameResolution.first
-        : active;
+    final DisplayMode mostOptimalMode =
+        sameResolution.isNotEmpty ? sameResolution.first : active;
 
     /// This setting is per session.
     /// Please ensure this was placed with initState of your root widget.
