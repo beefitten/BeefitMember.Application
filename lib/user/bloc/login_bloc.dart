ef503c8c2048836ab48eddbd5171a6bad2bfe06c
Uniowl <AlexSmith@live.dk>
2021-10-08 15:19:10 +0200

    Prettyfied

--- lib/user/bloc/login_bloc.dart
+++ lib/user/bloc/login_bloc.dart
@@ -3,24 +3,24 @@ import 'package:beefitmember_application/user/bloc/login_state.dart';
 import 'package:beefitmember_application/user/service/login_service.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class LoginBLoc extends Bloc<LoginEvents, LoginState>{
+class LoginBLoc extends Bloc<LoginEvents, LoginState> {
   LoginService service;
   LoginBLoc(LoginState initialState, this.service) : super(initialState);
 
   @override
-  Stream<LoginState> mapEventToState(LoginEvents event) async*{
-    if(event is StartEvent) {
+  Stream<LoginState> mapEventToState(LoginEvents event) async* {
+    if (event is StartEvent) {
       yield LoginInitState();
-    }
-    else if(event is LoginButtonPressed){
+    } else if (event is LoginButtonPressed) {
       yield LoginLoadingState();
-      await service.login(event.email, event.password);
+      var token = await service.login(event.email, event.password);
 
-      yield UserLoginSuccessState();
-    }
-    else {
+      if (token == "Error")
+        yield LoginErrorState(message: "Fail: Wrong Password");
+      else
+        yield UserLoginSuccessState();
+    } else {
       yield LoginErrorState(message: "Login error");
     }
-
   }
 }
