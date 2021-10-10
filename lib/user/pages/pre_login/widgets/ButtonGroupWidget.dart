2f7644331d3a88334fb6389217922d6c6cb51f21
Uniowl <mobilerobotcat@gmail.com>
2021-10-10 16:46:38 +0200

    Created customIconButton
    These are used to create the "sign in with facebook" and "..appleID" buttons

--- lib/user/pages/pre_login/widgets/ButtonGroupWidget.dart
+++ lib/user/pages/pre_login/widgets/ButtonGroupWidget.dart
@@ -5,42 +5,48 @@ class ButtonGroupWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          children: [
-            Expanded(
-              child: CustomButton(
-                "Sign in with e-mail",
-                () => {Navigator.pushNamed(context, '/login')},
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(24, 0, 24, 0),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: CustomButton(
+                    "Sign in with e-mail",
+                    () => {Navigator.pushNamed(context, '/login')},
+                  ),
+                ),
               ),
-            ),
-          ],
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            Expanded(
-              child: CustomButton(
-                "Facebook",
-                () => {},
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Expanded(
+                child: CustomIconButton(
+                  Icons.facebook_sharp,
+                  () => {},
+                ),
               ),
-            ),
-            Expanded(
-              child: CustomButton(
-                "AppleID",
-                () => {},
+              Expanded(
+                child: CustomIconButton(
+                  Icons.alarm,
+                  () => {},
+                ),
               ),
-            ),
-          ],
-        ),
-      ],
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
 
-//TODO: Delete this when "lib/shared/widgets/button.dart" has been created
+//TODO: Move this to SharedWidgets and convince group to start using
 class CustomButton extends StatelessWidget {
   final VoidCallback _onPressed;
   final String _text;
@@ -49,18 +55,41 @@ class CustomButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return RaisedButton(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      onPressed: this._onPressed,
+      padding: EdgeInsets.all(20),
+      color: Color.fromRGBO(6, 62, 249, 1),
+      textColor: Colors.white,
+      child: Text(this._text),
+    );
+  }
+}
+
+class CustomIconButton extends StatelessWidget {
+  final IconData _icon;
+  final VoidCallback _onPressed;
+
+  CustomIconButton(this._icon, this._onPressed);
+
+  @override
+  Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.fromLTRB(24.0, 16.0, 24.0, 0),
-      // ignore: deprecated_member_use
+      padding: EdgeInsets.fromLTRB(12.0, 12.0, 12.0, 0),
       child: RaisedButton(
         shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(5),
+          borderRadius: BorderRadius.circular(10),
         ),
         onPressed: this._onPressed,
-        padding: EdgeInsets.all(12),
-        color: Color.fromRGBO(6, 62, 249, 1),
-        textColor: Colors.white,
-        child: Text(this._text),
+        color: Colors.white,
+        textColor: Colors.black,
+        child: IconButton(
+          icon: Icon(this._icon),
+          onPressed: this._onPressed,
+          iconSize: 35,
+        ),
       ),
     );
   }
