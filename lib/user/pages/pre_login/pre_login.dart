2f9092621f14281b6bdc5d511625439a4d15e082
Uniowl <mobilerobotcat@gmail.com>
2021-10-11 12:50:11 +0200

    adjusted size of bottom text

--- lib/user/pages/pre_login/pre_login.dart
+++ lib/user/pages/pre_login/pre_login.dart
@@ -24,12 +24,17 @@ class _PreLoginState extends State<PreLogin> {
           CarouselWidget(),
           Expanded(child: ButtonGroupWidget()),
           Padding(
-            padding: const EdgeInsets.all(20.0),
+            padding: const EdgeInsets.only(bottom: 15),
             child: Container(
               alignment: Alignment.bottomCenter,
               child: Text(
                 "By continuing you agree to Beefits Terms of Service \n and Privacy Policy",
                 textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black38,
+                  fontSize: MediaQuery.of(context).size.height *
+                      0.018, //Scales text to screensize. Adjust the integer to adjust size
+                ),
               ),
             ),
           ),
