46882e48a5d08dc2a0b993e1e77a93c999f06f8e
Uniowl <AlexSmith@live.dk>
2021-10-13 12:25:49 +0200

    Changed login to return the responseJSON

--- lib/user/service/login_service.dart
+++ lib/user/service/login_service.dart
@@ -23,17 +23,14 @@ class LoginService {
       body: bodyJson,
     );
 
-    if (response.statusCode == 200){
+    if (response.statusCode == 200) {
       var responseJson = json.decode(response.body);
       var token = responseJson['token'].toString();
-      var name = responseJson['name'].toString();
 
       await SecureStorage.setToken(token);
 
-      return token;
-    }
-    else
-    {
+      return responseJson;
+    } else {
       return "error";
     }
   }
