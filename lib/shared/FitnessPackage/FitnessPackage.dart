4fe80549e2523457750cb0d39f18ac5cc6d8cc92
Uniowl <AlexSmith@live.dk>
2021-10-13 15:03:19 +0200

    Created FitnessPackage

--- lib/shared/FitnessPackage/FitnessPackage.dart
+++ lib/shared/FitnessPackage/FitnessPackage.dart
@@ -1,23 +1,31 @@
 class FitnessPackage {
   static final FitnessPackage _instance = FitnessPackage._internal();
 
-  // using a factory is important
-  // because it promises to return _an_ object of this type
-  // but it doesn't promise to make a new one.
+  // All attributes are initialized with default values
+  String name = "Default Fitness Center";
+  String primaryColor = "0x063EF9";
+  String secondaryColor = "0xF7F7FC";
+  String logo = "noPath";
+
+  List<String> features = []; // Could also be a list of ints
+
   factory FitnessPackage() {
     return _instance;
   }
 
   // This named constructor is the "real" constructor
-  // It'll be called exactly once, by the static property assignment above
-  // it's also private, so it can only be called in this class
   FitnessPackage._internal() {
     // initialization logic
   }
 
   // Get the latest fitness package configuration
-  void update() {
-    // Send POST to MS-FitnessPackage
-    // Save values in attributes
+  void update(dynamic json) {
+    // Save values in class attributes
+    name = json['Name'].toString();
+    primaryColor = json['PrimaryColor']
+        .toString(); // Might need to be changed to a "Color"
+    secondaryColor = json['SeconderyColor'].toString();
+    logo = json['Logo'].toString();
+    features = json['Features'];
   }
 }
