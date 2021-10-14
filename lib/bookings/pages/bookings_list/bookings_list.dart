0a4a94b9379a282b47e44fff48330e90add2d794
Thanh <thph@stofa.dk>
2021-10-14 11:03:43 +0200

    classes in bookingsdone but still need dot indicator

--- lib/bookings/pages/bookings_list/bookings_list.dart
+++ lib/bookings/pages/bookings_list/bookings_list.dart
@@ -1,3 +1,4 @@
+import 'package:beefitmember_application/bookings/pages/bookings_list/booking_card.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -19,16 +20,12 @@ class _ClassesState extends State<Classes> {
 
   @override
   Widget build(BuildContext context) {
-    final green = Color.fromRGBO(0, 186, 136, 1);
-    final blue = Color.fromRGBO(6, 62, 249, 1);
-
-    String className = "Fit Fat Fuck";
-    String timeStart = "11:30";
+    String topicHeaderText = "Bookings";
+    String timeStart = "10:30";
     String timeEnd = "12:45";
+    String className = "Bike Standard";
     String place = "Frederiksbjerg";
     String city = "København";
-
-    String topicHeaderText = "Bookings";
 
     final topic = Text("$topicHeaderText",
         textAlign: TextAlign.center,
@@ -39,77 +36,22 @@ class _ClassesState extends State<Classes> {
         ));
 
     final generalText = (String txt) => Padding(
-      padding: const EdgeInsets.fromLTRB(8, 0, 0, 0),
-      child: Text(
+          padding: const EdgeInsets.fromLTRB(8, 0, 0, 0),
+          child: Text(
             txt,
             style: TextStyle(
               fontWeight: FontWeight.bold,
               fontSize: 16.0,
             ),
           ),
-    );
-
-    final generalHeaderText = (String txt) => Text(
-          txt,
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 16.0,
-          ),
         );
 
-    final subtext = (String txt) => Padding(
-      padding: EdgeInsets.only(bottom: 6.0),
-      child: Text(
-            txt,
-            style:
-                TextStyle(fontSize: 15.0, color: Color.fromRGBO(78, 75, 102, 1)),
-          ),
-    );
-
-    final locationText = (String txt) => Text(
-          txt,
-          style: TextStyle(
-              fontSize: 13.0, color: Color.fromRGBO(138, 141, 178, 1)),
-        );
-
-    final bookBtn = ElevatedButton(
-      onPressed: () {
-        handleBook();
-      },
-      child: Text(this._isBooked ? "Booked" : "Book"),
-      style: ElevatedButton.styleFrom(primary: _isBooked ? green : blue),
-      
-    );
-
-    final list = Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Card(
-            child: ListTile(
-                title: generalHeaderText("$className"),
-                subtitle: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Column(
-                      children: [
-                        Row(
-                          children: [subtext("$timeStart - $timeEnd")],
-                        ),
-                        locationText("$place, $city")
-                      ],
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                    ),
-                    SizedBox(width: 120.0,),
-                    SizedBox(child: bookBtn, width: 80.0)
-                  ],
-                ),
-                tileColor: Color.fromRGBO(247, 247, 252, 1)),
-          ),
-        ),
-      ],
-    );
+    final card = BookingCard(
+      className: className,
+      timeStart: timeStart, 
+      timeEnd: timeEnd,
+      place: place,
+      city: city,);
 
     return Scaffold(
       backgroundColor: Colors.white,
@@ -117,13 +59,17 @@ class _ClassesState extends State<Classes> {
         shrinkWrap: true,
         children: <Widget>[
           topic,
+          generalText("Filters"),
           generalText("Today"),
-          list,
+          card,
           generalText("Tomorrow"),
-          list,
-          list,
-          list,
-          list,
+          card,
+          card,
+          card,
+          card,
+          card,
+          card,
+          card,
         ],
       ),
     );
