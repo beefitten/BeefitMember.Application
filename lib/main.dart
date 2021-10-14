0e53f252afb43fc96fef53bcd936a64937f422c2
Thanh <thph@stofa.dk>
2021-10-14 13:32:03 +0200

    removed unused import

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,14 @@
 import 'package:beefitmember_application/bookings/pages/bookings_list/bookings_list.dart';
-import 'package:beefitmember_application/bookings/pages/yourbookings/your_bookings.dart';
 import 'package:beefitmember_application/user/bloc/login_bloc.dart';
 import 'package:beefitmember_application/user/bloc/login_state.dart';
 import 'package:beefitmember_application/user/pages/Contact.dart';
 
 import 'package:beefitmember_application/navigation/nav.dart';
 import 'package:beefitmember_application/user/pages/login/login_form.dart';
-import 'package:beefitmember_application/user/pages/pre_login/pre_login.dart';
 import 'package:beefitmember_application/user/service/login_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'user/pages/pre_login/pre_login.dart';
 
 void main() {
   runApp(MyApp());
