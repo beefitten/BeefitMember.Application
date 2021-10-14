9cab5fd33b75a9e3172ee8ecbd5f8a442040cf61
Uniowl <AlexSmith@live.dk>
2021-10-14 14:54:42 +0200

    Rerouted root to preLogin

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'Beefit Member',
         routes: {
-          '/': (context) => Classes(),
+          '/': (context) => PreLogin(),
           '/contacts': (context) => Contact(),
           '/login': (context) => Login(),
           '/nav': (context) => Nav(),
