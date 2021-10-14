8c463be23f62cf019a4be972361af44b519707c7
Uniowl <AlexSmith@live.dk>
2021-10-14 17:41:05 +0200

    Gave padding responsibility to parent column

--- lib/training_progression/widgets/image_box.dart
+++ lib/training_progression/widgets/image_box.dart
@@ -7,14 +7,11 @@ class ImageBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(10.0),
-        child: Image(
-          image: NetworkImage(
-            _assetPath,
-          ),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10.0),
+      child: Image(
+        image: NetworkImage(
+          _assetPath,
         ),
       ),
     );
