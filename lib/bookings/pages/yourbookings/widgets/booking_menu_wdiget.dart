152aa8a45019ed8ede17ad0cfdb11e66c879da7f
Uniowl <AlexSmith@live.dk>
2021-10-15 09:15:44 +0200

    pretty

--- lib/bookings/pages/yourbookings/widgets/booking_menu_wdiget.dart
+++ lib/bookings/pages/yourbookings/widgets/booking_menu_wdiget.dart
@@ -1,4 +1,6 @@
 import 'package:beefitmember_application/bookings/pages/yourbookings/your_bookings.dart';
+import 'package:beefitmember_application/shared/widgets/texts.dart';
+import 'package:beefitmember_application/training_progression/widgets/title_banner.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -6,29 +8,28 @@ class BookingMenuWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          appBar: CustomAppBar("Bookings"),
-          body: TabBarView(
-            children: [
-              YourBookings(),
-              Text("Sut"),
-              Text("Dut"),
-            ],
-          ),
+      length: 3,
+      child: Scaffold(
+        appBar: CustomAppBar("Bookings"),
+        body: TabBarView(
+          children: [
+            YourBookings(),
+            Text("Sut"),
+            Text("Dut"),
+          ],
         ),
+      ),
     );
   }
 }
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
-
   String _title;
 
   CustomAppBar(this._title);
 
   onItemTap(int index) {
-    switch(index) {
+    switch (index) {
       case 0:
         _title = "Bookings";
         break;
@@ -43,48 +44,32 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 50),
-            child: Text(
-              _title,
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: MediaQuery
-                    .of(context)
-                    .size
-                    .height * 0.025,
-                fontWeight: FontWeight.bold,
-              ),
+    return Column(children: [
+      TitleBanner(_title),
+      Padding(
+        padding: const EdgeInsets.only(left: 8, right: 8),
+        child: TabBar(
+            indicator: ShapeDecoration(
+              shape: StadiumBorder(),
+              color: Color.fromRGBO(95, 46, 234, 1),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 8, right: 8, top: 10),
-            child: TabBar(
-                indicator: ShapeDecoration(
-                  shape: StadiumBorder(),
-                  color: Color.fromRGBO(95, 46, 234, 1),
-                ),
-                labelStyle: TextStyle(
-                    color: Colors.white, fontWeight: FontWeight.bold),
-                //For Selected tab
-                unselectedLabelStyle: TextStyle(
-                  fontSize: 14,
-                ),
-                unselectedLabelColor: Colors.grey,
-                onTap: (index) {
-                  onItemTap(index);
-                },
-                tabs: [
-                  Tab(icon: Text("Bookings")),
-                  Tab(icon: Text("Classes")),
-                  Tab(icon: Text("Events")),
-                ]),
-          ),
-        ]
-    );
+            labelStyle:
+                TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            //For Selected tab
+            unselectedLabelStyle: TextStyle(
+              fontSize: 14,
+            ),
+            unselectedLabelColor: Colors.grey,
+            onTap: (index) {
+              onItemTap(index);
+            },
+            tabs: [
+              Tab(icon: Text("Bookings")),
+              Tab(icon: Text("Classes")),
+              Tab(icon: Text("Events")),
+            ]),
+      ),
+    ]);
   }
 
   @override
