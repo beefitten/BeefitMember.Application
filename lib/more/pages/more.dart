e44742a8da9f8bba12ab2ffc9eb61d9b3df066a9
Thanh <thph@stofa.dk>
2021-10-15 16:15:20 +0200

    structure on more

--- lib/more/pages/more.dart
+++ lib/more/pages/more.dart
@@ -1,4 +1,6 @@
 import 'package:beefitmember_application/bookings/pages/previewBookings/widgets/show_more_button.dart';
+import 'package:beefitmember_application/shared/FitnessPackage/FitnessPackage.dart';
+import 'package:beefitmember_application/training_progression/pages/training_progression.dart';
 import 'package:beefitmember_application/user/service/logout_service.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -11,13 +13,13 @@ class More extends StatefulWidget {
 class _MoreState extends State<More> {
   final _logOut = Text(
     'Log out',
-    style: TextStyle(fontSize: 20.0),
+    style: TextStyle(
+        fontSize: 20.0, color: Color(int.parse(FitnessPackage.secondaryColor))),
   );
 
   @override
   void initState() {
     super.initState();
-    build(context);
   }
 
   @override
@@ -27,10 +29,10 @@ class _MoreState extends State<More> {
 
   @override
   Widget build(BuildContext context) {
-
     final _logOutBtn = TextButton(
       style: ButtonStyle(
-        backgroundColor:  MaterialStateProperty.all(Color.fromRGBO(229, 235, 254, 1.0))),
+          backgroundColor:
+              MaterialStateProperty.all(Color.fromRGBO(229, 235, 254, 1.0))),
       child: _logOut,
       onPressed: () {
         LogoutService.logOut();
@@ -39,12 +41,13 @@ class _MoreState extends State<More> {
     );
 
     final headerGeneral = (String txt) => Padding(
-      padding: EdgeInsets.only(right: MediaQuery.of(context).size.width * 0.46),
-      child: Text(
-        "$txt",
-        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-      ),
-    );
+          padding:
+              EdgeInsets.only(right: MediaQuery.of(context).size.width * 0.44),
+          child: Text(
+            "$txt",
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          ),
+        );
 
     final membership = Container(
       child: Row(
@@ -52,20 +55,89 @@ class _MoreState extends State<More> {
       ),
     );
 
+    // final bigCard = Container(
+    //   child: ,
+    // );
+    final smallCard = Container();
+
     final more = Container(
-      child: headerGeneral("More"),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          headerGeneral("More"),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              // left column under more
+              Column(
+                children: [
+                  Text("data1")
+                ],
+              ),
+              // right column under more
+              Column(
+                children: [Text("data2")],
+              )
+            ],
+          )
+        ],
+      ),
+    );
+
+    final membershipCard = Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5.0),
+          color: Color.fromRGBO(247, 247, 252, 1) // whitelabelin color
+          ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("Logo"),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: Text("Medlemsskab A"),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: Text("Bindsperiode: 6 måneder"),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
     );
 
     return Scaffold(
         backgroundColor: Colors.white,
         body: Padding(
-          padding: EdgeInsets.only(left: MediaQuery.of(context).size.width *0.05),
+          padding: EdgeInsets.only(
+            left: MediaQuery.of(context).size.width * 0.05,
+          ),
           child: Container(
               child: ListView(
             children: [
               membership,
+              Padding(
+                padding: EdgeInsets.only(
+                    right: MediaQuery.of(context).size.width * 0.05,
+                    bottom: MediaQuery.of(context).size.height * 0.02),
+                child: membershipCard,
+              ),
               more,
-              _logOutBtn,
+              Padding(
+                padding: EdgeInsets.only(
+                    right: MediaQuery.of(context).size.width * 0.05),
+                child: _logOutBtn,
+              ),
             ],
           )),
         ));
