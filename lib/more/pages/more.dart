e1bd475e34789e8772dbf1a5ee7bd390a9943733
Thanh <thph@stofa.dk>
2021-10-15 13:59:14 +0200

    more startet

--- lib/more/pages/more.dart
+++ lib/more/pages/more.dart
@@ -1,8 +1,4 @@
-import 'package:beefitmember_application/bookings/pages/yourbookings/widgets/appointmentswidget.dart';
-import 'package:beefitmember_application/bookings/pages/yourbookings/widgets/booking_menu_widget.dart';
-import 'package:beefitmember_application/bookings/pages/yourbookings/widgets/bookingswidget.dart';
-import 'package:beefitmember_application/bookings/pages/yourbookings/widgets/find_classes_widget.dart';
-import 'package:beefitmember_application/shared/widgets/buttons.dart';
+import 'package:beefitmember_application/bookings/pages/previewBookings/widgets/show_more_button.dart';
 import 'package:beefitmember_application/user/service/logout_service.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -13,33 +9,65 @@ class More extends StatefulWidget {
 }
 
 class _MoreState extends State<More> {
+  final _logOut = Text(
+    'Log out',
+    style: TextStyle(fontSize: 20.0),
+  );
 
   @override
   void initState() {
     super.initState();
     build(context);
-    print("Init more");
   }
 
   @override
-  void dispose(){
+  void dispose() {
     super.dispose();
-    print("Dispose more");
   }
 
   @override
   Widget build(BuildContext context) {
+
+    final _logOutBtn = TextButton(
+      style: ButtonStyle(
+        backgroundColor:  MaterialStateProperty.all(Color.fromRGBO(229, 235, 254, 1.0))),
+      child: _logOut,
+      onPressed: () {
+        LogoutService.logOut();
+        Navigator.pop(context, '/nav');
+      },
+    );
+
+    final headerGeneral = (String txt) => Padding(
+      padding: EdgeInsets.only(right: MediaQuery.of(context).size.width * 0.46),
+      child: Text(
+        "$txt",
+        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+      ),
+    );
+
+    final membership = Container(
+      child: Row(
+        children: [headerGeneral("Membership"), ShowMore()],
+      ),
+    );
+
+    final more = Container(
+      child: headerGeneral("More"),
+    );
+
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Container(
-        child: Center(
-            child: TextButton(
-              child: Text('Log out', style: TextStyle(fontSize: 20.0),),
-              onPressed: () {
-                LogoutService.logOut();
-                Navigator.pop(context, '/nav');
-              },
-        ), )
-    ));
+        backgroundColor: Colors.white,
+        body: Padding(
+          padding: EdgeInsets.only(left: MediaQuery.of(context).size.width *0.05),
+          child: Container(
+              child: ListView(
+            children: [
+              membership,
+              more,
+              _logOutBtn,
+            ],
+          )),
+        ));
   }
 }
