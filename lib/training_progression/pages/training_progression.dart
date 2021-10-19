0af7d3974a068af0cb6a4ebd61593735548ed35c
Uniowl <mobilerobotcat@gmail.com>
2021-10-19 15:36:52 +0200

    Added Modal Bottom sheet to Arnold. WIP

--- lib/training_progression/pages/training_progression.dart
+++ lib/training_progression/pages/training_progression.dart
@@ -16,24 +16,42 @@ class _TrainingProgressionState extends State<TrainingProgression> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(left: 20.0, right: 20.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          TitleBanner("Weight Goal"),
-          ImageBox(
-            "https://2.bp.blogspot.com/-q3qJmAW1T_0/Vty1RAbGe4I/AAAAAAAAAlc/otdCoDEdMl8/s640/Arnold-Schwarzenegger-Champion-BodyBuilder.jpg",
-          ),
-          TitleText("Setup a weight goal"),
-          Container(
-            width: 100,
-            child: DescriptionText(
-                "Get motivated by setting up a few simple weekly goals, and the app will keep track of whether you archive the goals every week. "),
-          ),
-          CustomButton("Setup new weight goal", () => {},
-              Color(int.parse(FitnessPackage.secondaryColor))),
-        ],
+      child: Scaffold(
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            TitleBanner("Weight Goal"),
+            ImageBox(
+              "https://2.bp.blogspot.com/-q3qJmAW1T_0/Vty1RAbGe4I/AAAAAAAAAlc/otdCoDEdMl8/s640/Arnold-Schwarzenegger-Champion-BodyBuilder.jpg",
+            ),
+            TitleText("Setup a weight goal"),
+            Container(
+              width: 100,
+              child: DescriptionText(
+                  "Get motivated by setting up a few simple weekly goals, and the app will keep track of whether you archive the goals every week. "),
+            ),
+            CustomButton("Setup new weight goal", () => _onButtonPressed(),
+                Color(int.parse(FitnessPackage.secondaryColor))),
+          ],
+        ),
       ),
+    );
+  }
+
+  _onButtonPressed() {
+    showModalBottomSheet(
+      context: (context),
+      builder: (context) {
+        return Column(
+          children: [
+            ListTile(
+              leading: Icon(Icons.face),
+              title: Text("Test"),
+            )
+          ],
+        );
+      },
     );
   }
 }
