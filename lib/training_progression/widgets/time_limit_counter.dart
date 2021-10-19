2f9ea0a52ffeeddb469b1598a90a0d4311626e4f
Uniowl <mobilerobotcat@gmail.com>
2021-10-19 19:47:43 +0200

    Fixed text not aligning left

--- lib/training_progression/widgets/time_limit_counter.dart
+++ lib/training_progression/widgets/time_limit_counter.dart
@@ -13,6 +13,7 @@ class _TimeLimitCounterState extends State<TimeLimitCounter> {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         H3Text("Enable time limit"),
         DescriptionText(
