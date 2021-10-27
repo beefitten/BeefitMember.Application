239291fa77d0cf6df989a546cfcf9c8564a3c638
Thanh <thph@stofa.dk>
2021-10-27 15:23:36 +0200

    added preview widget BS-197 + BS-197 --> adding properties to fitnessPackage

--- lib/shared/FitnessPackage/FitnessPackage.dart
+++ lib/shared/FitnessPackage/FitnessPackage.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 class FitnessPackage {
   static final FitnessPackage _instance = FitnessPackage._internal();
 
@@ -5,6 +7,24 @@ class FitnessPackage {
   static String name = "Default Fitness Center";
   static String primaryColor = "0x063EF9";
   static String secondaryColor = "0xF7F7FC";
+  static String thirdColor = "";
+  static Color backgroundColor = Colors.white;
+  static String generalFond = "";
+
+  static String H1 = "18";
+  static String H1Weight = "";
+  static String H1FontFamily = "";
+
+  static String H2 = "16";
+  static String H2Weight = "";
+  static String H2FontFamily = "";
+
+  static String H3 = "14";
+  static String H3Weight = "";
+  static String H3FontFamily = "";
+
+  static String borderRadius = "";
+  static double elevation = 3;
   static String logo = "noPath";
 
   static List<int> features = []; // Could also be a list of ints
@@ -22,7 +42,8 @@ class FitnessPackage {
   void update(dynamic json) {
     // Save values in class attributes
     name = json['name'].toString();
-    primaryColor = json['primaryColor'].toString(); // Might need to be changed to a "Color"
+    primaryColor = json['primaryColor']
+        .toString(); // Might need to be changed to a "Color"
     secondaryColor = json['secondaryColor'].toString();
     logo = json['logo'].toString();
 
