da26f42073442626537449f640e6f50a95e7e754
Uniowl <AlexSmith@live.dk>
2021-10-28 13:19:04 +0200

    Removed redundant

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,8 @@ import 'package:beefitmember_application/navigation/nav.dart';
 import 'package:beefitmember_application/user/pages/login/login_form.dart';
 import 'package:beefitmember_application/user/pages/pre_login/pre_login.dart';
 import 'package:beefitmember_application/user/service/login_service.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 import 'bookings/bloc/bookingsList/bookings_list_bloc.dart';
 import 'bookings/bloc/bookingsList/bookings_list_state.dart';
 import 'bookings/pages/yourbookings/your_bookings.dart';
