dd1ffabc5300ff7d087c31624b7d55a91f0608ce
Uniowl <AlexSmith@live.dk>
2021-10-29 09:55:30 +0200

    Removed TODOS that would never be completed

--- lib/overview/widgets/app_bar_curve.dart
+++ lib/overview/widgets/app_bar_curve.dart
@@ -4,10 +4,10 @@ class AppBarCurve extends ShapeBorder {
   @override
   Path getOuterPath(Rect rect, {TextDirection? textDirection}) {
     var path = Path();
-    
+
     path.moveTo(0, rect.height * 0.80);
     path.quadraticBezierTo(
-        rect.width / 2, rect.height, rect.width, rect.height*0.80);
+        rect.width / 2, rect.height, rect.width, rect.height * 0.80);
     path.lineTo(rect.width, 0);
     path.lineTo(0, 0);
     path.close();
@@ -16,22 +16,18 @@ class AppBarCurve extends ShapeBorder {
   }
 
   @override
-  // TODO: implement dimensions
   EdgeInsetsGeometry get dimensions => throw UnimplementedError();
 
   @override
   Path getInnerPath(Rect rect, {TextDirection? textDirection}) {
-    // TODO: implement getInnerPath
     throw UnimplementedError();
   }
 
   @override
-  void paint(Canvas canvas, Rect rect, {TextDirection? textDirection}) {
-  }
+  void paint(Canvas canvas, Rect rect, {TextDirection? textDirection}) {}
 
   @override
   ShapeBorder scale(double t) {
-    // TODO: implement scale
     throw UnimplementedError();
   }
 }
