fbbd967d83995d09c4a9b1d0a328836ff9641143
Uniowl <mobilerobotcat@gmail.com>
2021-11-02 23:34:27 +0100

    Changed double to int

--- lib/training_progression/widgets/big_circle_chart.dart
+++ lib/training_progression/widgets/big_circle_chart.dart
@@ -4,7 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class BigCircleChart extends StatelessWidget {
-  final int _percentage;
+  final double _percentage;
 
   BigCircleChart(this._percentage);
 
