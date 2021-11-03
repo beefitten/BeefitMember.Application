f4346d7ef439543b3c3afcbaccdcbc641f96b2ec
Uniowl <AlexSmith@live.dk>
2021-11-03 16:17:46 +0100

    Changed color

--- lib/more/widgets/background_curve.dart
+++ lib/more/widgets/background_curve.dart
@@ -2,13 +2,12 @@ import 'package:beefitmember_application/shared/FitnessPackage/FitnessPackage.da
 import 'package:flutter/material.dart';
 
 class BackgroundCurve extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Container(
       height: MediaQuery.of(context).size.height * 0.17,
       decoration: BoxDecoration(
-          color: Color(int.parse(FitnessPackage.model.thirdColor)),
+          color: Color(int.parse(FitnessPackage.model.primaryColor)),
           borderRadius: new BorderRadius.vertical(
               bottom: new Radius.elliptical(
                   MediaQuery.of(context).size.width, 90))),
