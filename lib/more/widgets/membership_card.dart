fb92f717923c1342fe7227bfab6aae227ccf2c94
Uniowl <AlexSmith@live.dk>
2021-11-03 16:09:20 +0100

    Pretty

--- lib/more/widgets/membership_card.dart
+++ lib/more/widgets/membership_card.dart
@@ -1,47 +1,46 @@
 import 'package:beefitmember_application/shared/FitnessPackage/FitnessPackage.dart';
 import 'package:beefitmember_application/shared/user/user.dart';
+import 'package:beefitmember_application/shared/widgets/texts.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-
 final TextStyle fontFamily =
-      GoogleFonts.getFont(FitnessPackage.model.font.generalFont);
+    GoogleFonts.getFont(FitnessPackage.model.font.generalFont);
 
 class MemberShipCard extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(double.parse(FitnessPackage.model.borderRadius)),
+          borderRadius: BorderRadius.circular(
+              double.parse(FitnessPackage.model.borderRadius)),
           color: Color(int.parse(FitnessPackage.model.backgroundColor))),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(
-          children: [
-            Padding(
-              padding: EdgeInsets.only(
-                  right: MediaQuery.of(context).size.width * 0.04,
-                  left: MediaQuery.of(context).size.width * 0.02),
-              child: Icon(
-                Icons.beach_access,
-              ),
+      child: Row(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(
+                right: MediaQuery.of(context).size.width * 0.04,
+                left: MediaQuery.of(context).size.width * 0.02),
+            child: Icon(
+              Icons.beach_access,
             ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 8.0),
-                  child: Text("Membership: " + User.subscription, style: fontFamily,),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: H3Text(
+                  "Membership: ${User.subscription}",
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 8.0),
-                  child: Text("Primary Gym: " + User.primaryGym, style: fontFamily),
-                )
-              ],
-            )
-          ],
-        ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: H3Text("Primary Gym: ${User.primaryGym}"),
+              )
+            ],
+          )
+        ],
       ),
     );
   }
